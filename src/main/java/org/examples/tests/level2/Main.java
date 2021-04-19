f7c04c191c5da5db375b4c21ee8dae15ea41cb32
Vyacheslav-Lapin <Vyacheslav.Lapin@gmail.com>
2021-04-19 11:24:20 +0300

    Initial commit

---
+++ src/main/java/org/examples/tests/level2/Main.java
@@ -0,0 +1,13 @@
+package org.examples.tests.level2;
+
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+@Slf4j
+public class Main {
+
+  public static void main(String... __) {
+    val s = "Hello, World!";
+    System.out.println("\"Hello, World!\" = " + s);
+  }
+}
